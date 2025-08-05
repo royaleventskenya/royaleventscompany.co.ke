515bdaef887e99140239b1a67ac8c544efe0c955
Robinson Omare <robinsonomare12@gmail.com>
2025-08-05 16:22:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+function showNextSlide() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+setInterval(showNextSlide, 4000);
 
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("active");
+}
