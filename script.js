8400b11b1332360537483010e52a8fee1e9e1659
Robinson Omare <robinsonomare12@gmail.com>
2025-08-05 16:40:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-let slides = document.querySelectorAll(".slide");
-let index = 0;
-function showNextSlide() {
-  slides[index].classList.remove("active");
-  index = (index + 1) % slides.length;
-  slides[index].classList.add("active");
-}
-setInterval(showNextSlide, 4000);
+<script>
+    let slides = document.querySelectorAll(".slide");
+    let index = 0;
 
-function toggleMenu() {
-  document.getElementById("navLinks").classList.toggle("active");
-}
+    function showNextSlide() {
+      slides[index].classList.remove("active");
+      index = (index + 1) % slides.length;
+      slides[index].classList.add("active");
+    }
+
+    setInterval(showNextSlide, 4000);
+  </script>
+  <!-- Lightbox2 JS -->
+<script src="https://cdn.jsdelivr.net/npm/lightbox2@2.11.4/dist/js/lightbox.min.js"></script>
+<script>
+  function toggleMenu() {
+    document.getElementById("navLinks").classList.toggle("active");
+  }
+</script>
+
+
+Copied from: Editing royaleventscompany.co.ke/index.html at main · royaleventskenya/royaleventscompany.co.ke - <https://github.com/royaleventskenya/royaleventscompany.co.ke/edit/main/index.html>
